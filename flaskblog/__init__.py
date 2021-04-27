46bb98ff4b61aa92f09baebf61034999e3292331
Kruti Amrutiya <krutiamrutiya1998@gmail.com>
2021-04-27 15:52:26 +0530

    change the structure of project by using blueprints

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,25 +1,35 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from dotenv import load_dotenv
+from flaskblog.config import Config
+
 
 load_dotenv()
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '70e08850fd508457fbbc861cccbea404'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kruti@1210@localhost:5432/flask'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASS')
-mail = Mail(app)
+mail = Mail()
 
-from flaskblog import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
